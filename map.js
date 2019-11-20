ee3b799e05296064df4336ad5dc2bf3dc9bb8b52
yangzy0812 <yangzy0812@users.noreply.github.com>
2019-11-20 17:25:42 -0500

    midreview

---
+++ map.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoieml5dWFuIiwiYSI6ImNpc2hxaTU5OTAwNzIydHBpbTVzZ3drYWQifQ.GvBjoYU2QIeRcd2FEDeyMw';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/ziyuan/ck1v1kez87o471cqm577t0lqe'
+});
